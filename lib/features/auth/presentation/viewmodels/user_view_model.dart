df05e5050c2e21c7bb69ecf880b5e70d9b9e16d7
Shreyash-Adhikari1 <adhikarishreyash@gmail.com>
2026-01-01 16:06:12 +0545

    user View Model Made

---
+++ lib/features/auth/presentation/viewmodels/user_view_model.dart
@@ -0,0 +1,79 @@
+import 'package:artsphere/features/auth/domain/usecases/login_usecase.dart';
+import 'package:artsphere/features/auth/domain/usecases/register_usecase.dart';
+import 'package:artsphere/features/auth/presentation/state/user_state.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// user view model provider
+final userViewModelProvider= NotifierProvider<UserViewModel, UserState>(
+  ()=>UserViewModel()
+);
+
+class UserViewModel extends Notifier<UserState> {
+  late final RegisterUsecase _registerUsecase;
+  late final LoginUsecase _loginUsecase;
+
+  @override
+  build() {
+    _registerUsecase = ref.read(registerUsecaseProvider);
+    _loginUsecase = ref.read(loginUsecaseProvider);
+    return UserState();
+  }
+  // register method 
+  Future<void> register({
+    required String fullName,
+    required String email,
+    required String password,
+    String? address,
+    String? phoneNumber,
+  }) async {
+    state = state.copyWith(status: UserStatus.loading);
+    final registerParams = RegisterUsecaseParams(
+      fullName: fullName,
+      email: email,
+      password: password,
+      address: address,
+      phoneNumber: phoneNumber,
+    );
+    final result = await _registerUsecase.call(registerParams);
+    result.fold((failure){
+      state.copyWith(
+        status: UserStatus.error,
+        errorMessage: failure.message
+      );
+    },(isRegistered){
+      if (isRegistered) {
+        state=state.copyWith(status: UserStatus.registered);
+      }else{
+        state=state.copyWith(
+        status: UserStatus.error,
+        errorMessage: "registration failed"
+      );
+      }
+      
+    });
+  }
+
+
+  // Login Method
+  Future<void> login({
+    required String email,
+    required String password
+  })async{
+    state=state.copyWith(status: UserStatus.loading);
+    final loginParams=LoginUsecaseParams(email: email, password: password);
+    final result= await _loginUsecase(loginParams);
+
+    result.fold((failure){
+      state=state.copyWith(
+        status: UserStatus.error,
+        errorMessage: failure.message
+      );
+    }, 
+    (userEntity){
+      state=state.copyWith(
+        status: UserStatus.authenticated,
+        userEntity: userEntity,
+      );
+    });
+  }
+}
