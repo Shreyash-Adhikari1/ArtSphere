3b69a3c39a13d923a4cc7342397809ca1b56c032
Shreyash-Adhikari1 <adhikarishreyash@gmail.com>
2026-01-13 18:54:21 +0545

    Overrides in main providerScope for shared preferences operations

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:artsphere/app/app.dart';
-import 'package:artsphere/core/services/hive_service.dart';
+import 'package:artsphere/core/services/hive/hive_service.dart';
+import 'package:artsphere/core/services/storage/user_session_service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await HiveService().init();
-  runApp(ProviderScope(child: MyApp()));
+
+  // Shared Preferences object : because shared prefs is async and provider is sync 
+
+  // Shared Prefs object
+  final sharedPrefs= await SharedPreferences.getInstance();
+
+  runApp(ProviderScope(
+    overrides: [sharedPreferencesProvider.overrideWithValue(sharedPrefs)],
+    child: MyApp()));
 }
