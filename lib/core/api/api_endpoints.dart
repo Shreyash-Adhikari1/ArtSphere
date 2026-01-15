7b4b57369ed8923a0acda93b296748bc1654d7dc
Shreyash-Adhikari1 <adhikarishreyash@gmail.com>
2026-01-15 11:49:25 +0545

    Build runs on Physical Device

--- lib/core/api/api_endpoints.dart
+++ lib/core/api/api_endpoints.dart
@@ -2,11 +2,11 @@ class ApiEndpoints {
   ApiEndpoints._();
 
   // Base URL - change this for production
-  static const String baseUrl = 'http://10.0.2.2:5000/api';
+  static const String baseUrl = 'http://192.168.68.103:5000/api';
   //static const String baseUrl = 'http://localhost:3000/api/v1';
-  // For Android Emulator use: 'http://10.0.2.2:3000/api/v1'
+  // For Android Emulator use: 'http://10.0.2.2:5000/api'  //usually used
   // For iOS Simulator use: 'http://localhost:5000/api/v1'
-  // For Physical Device use your computer's IP: 'http://192.168.x.x:5000/api/v1'
+  // For Physical Device use your computer's IP: 'http://192.168.68.103:5000/api' // use for physical device
 
   static const Duration connectionTimeout = Duration(seconds: 30);
   static const Duration receiveTimeout = Duration(seconds: 30);
