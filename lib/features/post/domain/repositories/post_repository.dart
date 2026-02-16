4db9be6fc260c781dd2b64664bba56a0d0e0a081
Shreyash-Adhikari1 <adhikarishreyash@gmail.com>
2026-02-16 14:06:04 +0545

    Post Domain Layer Repo and usecase defined for Create Post

--- lib/features/post/domain/repositories/post_repository.dart
+++ lib/features/post/domain/repositories/post_repository.dart
@@ -3,11 +3,19 @@ import 'package:artsphere/features/post/domain/entities/post_entity.dart';
 import 'package:dartz/dartz.dart';
 
 abstract interface class IPostRepository {
-  Future<Either<Failure, bool>> createPost(PostEntity post);
+  Future<Either<Failure, PostEntity>> createPost({
+    required PostEntity post,
+    required String mediaPath,
+  });
+
   Future<Either<Failure, PostEntity>> likePost(String postId, String userId);
   Future<Either<Failure, PostEntity>> unlikePost(String postId, String userId);
+
   Future<Either<Failure, List<PostEntity>>> getMyPosts(String userId);
-  Future<Either<Failure, PostEntity>> deletePost(String postId);
+
+  Future<Either<Failure, bool>> deletePost(String postId);
+
+  // You can change these later when you design comment entity properly
   Future<Either<Failure, PostEntity>> commentOnPost(
     String postId,
     String userId,
