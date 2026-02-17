c0fc0afc6ea807f867d74620d31f370f44036104
Shreyash-Adhikari1 <adhikarishreyash@gmail.com>
2026-02-17 09:09:21 +0545

    All necessary post methods defined in domain layer repo

--- lib/features/post/domain/repositories/post_repository.dart
+++ lib/features/post/domain/repositories/post_repository.dart
@@ -1,5 +1,6 @@
 import 'package:artsphere/core/error/failures.dart';
 import 'package:artsphere/features/post/domain/entities/post_entity.dart';
+import 'package:artsphere/features/post/domain/usecases/edit_post_usecase.dart';
 import 'package:dartz/dartz.dart';
 
 abstract interface class IPostRepository {
@@ -8,12 +9,17 @@ abstract interface class IPostRepository {
     required String mediaPath,
   });
 
-  Future<Either<Failure, PostEntity>> likePost(String postId, String userId);
-  Future<Either<Failure, PostEntity>> unlikePost(String postId, String userId);
+  Future<Either<Failure, bool>> editPost(EditPostUsecaseParams params);
 
-  Future<Either<Failure, List<PostEntity>>> getMyPosts(String userId);
+  Future<Either<Failure, List<PostEntity>>> getMyPosts();
+  Future<Either<Failure, List<PostEntity>>> getUsersPosts(String userId);
+  Future<Either<Failure, List<PostEntity>>> getFeed();
+  Future<Either<Failure, List<PostEntity>>> getFollowingFeed();
 
   Future<Either<Failure, bool>> deletePost(String postId);
+
+  Future<Either<Failure, PostEntity>> likePost(String postId, String userId);
+  Future<Either<Failure, PostEntity>> unlikePost(String postId, String userId);
 
   // You can change these later when you design comment entity properly
   Future<Either<Failure, PostEntity>> commentOnPost(
