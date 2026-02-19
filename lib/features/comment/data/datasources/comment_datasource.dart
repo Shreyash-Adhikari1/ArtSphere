69a2453a2acb6bad47ade47fe2e10df184052809
Shreyash-Adhikari1 <adhikarishreyash@gmail.com>
2026-02-19 14:30:56 +0545

    Remote datasource implemented

--- lib/features/comment/data/datasources/comment_datasource.dart
+++ lib/features/comment/data/datasources/comment_datasource.dart
@@ -3,15 +3,19 @@ import 'package:artsphere/features/comment/data/models/create/create_comment_api
 
 abstract interface class ICommentRemoteDatasource {
   // Create
-  Future<CommentApiModel> createComment(CreateCommentApiModel comment);
+  Future<CommentApiModel> createComment(
+    String postId,
+    CreateCommentApiModel comment,
+  );
 
   // Get
+  Future<List<CommentApiModel>> getCommentsForPost(String postId);
   Future<List<CommentApiModel>> getCommentsByUser(String userId);
   Future<CommentApiModel> getCommentById(String commentId);
 
   // Like/Unlike
-  Future<CommentApiModel> likeComment(String commentId, String userId);
-  Future<CommentApiModel> unlikeComment(String commentId, String userId);
+  Future<bool> likeComment(String commentId);
+  Future<bool> unlikeComment(String commentId);
 
   // Delete
   Future<bool> deleteComment(String commentId);
--- lib/features/comment/data/datasources/remote/comment_remote_datasource.dart
+++ lib/features/comment/data/datasources/remote/comment_remote_datasource.dart
@@ -1,8 +1,10 @@
 import 'package:artsphere/core/api/api_client.dart';
+import 'package:artsphere/core/api/api_endpoints.dart';
 import 'package:artsphere/core/services/storage/token_service.dart';
 import 'package:artsphere/features/comment/data/datasources/comment_datasource.dart';
 import 'package:artsphere/features/comment/data/models/comment_api_model.dart';
 import 'package:artsphere/features/comment/data/models/create/create_comment_api_model.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final commentRemoteDatasourceProvider = Provider<ICommentRemoteDatasource>((
@@ -24,39 +26,101 @@ class CommentRemoteDatasource implements ICommentRemoteDatasource {
     required TokenService tokenservice,
   }) : _apiClient = apiClient,
        _tokenService = tokenservice;
+
+  // Methods Implementation
   @override
-  Future<CommentApiModel> createComment(CreateCommentApiModel comment) {
-    // TODO: implement createComment
-    throw UnimplementedError();
+  Future<CommentApiModel> createComment(
+    String postId,
+    CreateCommentApiModel comment,
+  ) async {
+    final token = _tokenService.getToken();
+
+    final response = await _apiClient.post(
+      ApiEndpoints.createComment(postId),
+      data: comment.toJson(),
+      options: Options(
+        headers: {'Authorization': 'Bearer $token'},
+        contentType: Headers.jsonContentType,
+      ),
+    );
+
+    if (response.data['success'] == true) {
+      final commentJson = Map<String, dynamic>.from(response.data["comment"]);
+      return CommentApiModel.fromJson(commentJson);
+    }
+
+    throw Exception(response.data["message"] ?? "Failed to create comment");
   }
 
   @override
-  Future<CommentApiModel> getCommentById(String commentId) {
+  Future<List<CommentApiModel>> getCommentsForPost(String postId) async {
+    final token = _tokenService.getToken();
+    final response = await _apiClient.get(
+      ApiEndpoints.getCommentsForPost(postId),
+      options: Options(headers: {'Authorization': 'Bearer $token'}),
+    );
+    if (response.data["success"] == true) {
+      final List commentData = response.data["comments"] as List;
+      final comments = commentData
+          .map(
+            (json) => CommentApiModel.fromJson(Map<String, dynamic>.from(json)),
+          )
+          .toList();
+      return comments;
+    }
+    throw Exception(
+      response.data["message"] ?? "Failed to get comments for post",
+    );
+  }
+
+  @override
+  Future<CommentApiModel> getCommentById(String commentId) async {
     // TODO: implement getCommentById
     throw UnimplementedError();
   }
 
   @override
-  Future<List<CommentApiModel>> getCommentsByUser(String userId) {
+  Future<List<CommentApiModel>> getCommentsByUser(String userId) async {
     // TODO: implement getCommentsByUser
     throw UnimplementedError();
   }
 
   @override
-  Future<CommentApiModel> likeComment(String commentId, String userId) {
-    // TODO: implement likeComment
-    throw UnimplementedError();
+  Future<bool> likeComment(String commentId) async {
+    final token = _tokenService.getToken();
+    final response = await _apiClient.post(
+      ApiEndpoints.likeComment(commentId),
+      options: Options(headers: {'Authorization': "Bearer $token"}),
+    );
+    if (response.data["success"] == true) {
+      return true;
+    }
+    throw Exception(response.data["message"] ?? "Failed to like comment");
   }
 
   @override
-  Future<CommentApiModel> unlikeComment(String commentId, String userId) {
-    // TODO: implement unlikeComment
-    throw UnimplementedError();
+  Future<bool> unlikeComment(String commentId) async {
+    final token = _tokenService.getToken();
+    final response = await _apiClient.post(
+      ApiEndpoints.unlikeComment(commentId),
+      options: Options(headers: {'Authorization': "Bearer $token"}),
+    );
+    if (response.data["success"] == true) {
+      return true;
+    }
+    throw Exception(response.data["message"] ?? "Failed to unlike comment");
   }
 
   @override
-  Future<bool> deleteComment(String commentId) {
-    // TODO: implement deleteComment
-    throw UnimplementedError();
+  Future<bool> deleteComment(String commentId) async {
+    final token = _tokenService.getToken();
+    final response = await _apiClient.delete(
+      ApiEndpoints.deleteComment(commentId),
+      options: Options(headers: {'Authorization': "Bearer $token"}),
+    );
+    if (response.data["success"] == true) {
+      return true;
+    }
+    throw Exception(response.data["message"] ?? "Failed to delete comment");
   }
 }
