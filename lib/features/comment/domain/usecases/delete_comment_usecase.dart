197ea1b1648d62977008774f7ec07f213a4a69f3
Shreyash-Adhikari1 <adhikarishreyash@gmail.com>
2026-02-20 20:28:21 +0545

    get comments usecase added || delete comment usecase provider added

--- lib/features/comment/domain/usecases/delete_comment_usecase.dart
+++ lib/features/comment/domain/usecases/delete_comment_usecase.dart
@@ -1,8 +1,10 @@
 import 'package:artsphere/core/error/failures.dart';
 import 'package:artsphere/core/usecases/app_usecase.dart';
+import 'package:artsphere/features/comment/data/repositories/comment_repository.dart';
 import 'package:artsphere/features/comment/domain/repositories/comment_repository.dart';
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class DeleteCommentUsecaseParams extends Equatable {
   final String commentId;
@@ -10,6 +12,12 @@ class DeleteCommentUsecaseParams extends Equatable {
   @override
   List<Object?> get props => [commentId];
 }
+
+final deleteCommentUsecaseProvider = Provider<DeleteCommentUsecase>((ref) {
+  return DeleteCommentUsecase(
+    commentRepository: ref.read(commentRepositoryProvider),
+  );
+});
 
 class DeleteCommentUsecase
     implements UsecaseWithParams<bool, DeleteCommentUsecaseParams> {
